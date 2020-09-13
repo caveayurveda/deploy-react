7987797a34c08cac439d673e6ad468c26acebe63
Umakant <umakantvashishtha98@gmail.com>
2020-09-13 23:52:01 +0530

    Updates

---
+++ precache-manifest.7b246c6d012ebea68ddec3d55f1f8a33.js
@@ -0,0 +1,378 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f81cddfb7a04cbe40bdc811f32afffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6462246145187e9569a7",
+    "url": "/static/css/2.b81ec60b.chunk.css"
+  },
+  {
+    "revision": "a1323bb2e1aa668f494c",
+    "url": "/static/css/main.f7ab176c.chunk.css"
+  },
+  {
+    "revision": "6462246145187e9569a7",
+    "url": "/static/js/2.e70447eb.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.e70447eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1323bb2e1aa668f494c",
+    "url": "/static/js/main.dbf9f173.chunk.js"
+  },
+  {
+    "revision": "f4a3766710e203b7dcb8",
+    "url": "/static/js/runtime-main.2c036d1a.js"
+  },
+  {
+    "revision": "711b9a5600e0f1bd99be26c0f0b14f81",
+    "url": "/static/media/AshwagandhaSlide1.711b9a56.png"
+  },
+  {
+    "revision": "46c68e23b45d6016d9927bb4c843be15",
+    "url": "/static/media/AshwagandhaSlide2.46c68e23.png"
+  },
+  {
+    "revision": "0090a99cfd0b7add82870adb750914e9",
+    "url": "/static/media/AshwagandhaSlide3.0090a99c.png"
+  },
+  {
+    "revision": "e686749ea0c80bea2af1cd8e7f2d5b57",
+    "url": "/static/media/AshwagandhaSlide4.e686749e.png"
+  },
+  {
+    "revision": "d674249152c4feb41bb3080455cd9741",
+    "url": "/static/media/AshwagandhaSlide5.d6742491.png"
+  },
+  {
+    "revision": "bdcf05f89b438e915d5e50bb8f5a537f",
+    "url": "/static/media/AshwagandhaSlide6.bdcf05f8.jpg"
+  },
+  {
+    "revision": "f92096efc9d69be8f27db6f1915a734d",
+    "url": "/static/media/GMp.f92096ef.png"
+  },
+  {
+    "revision": "6a970a5aa4585377cee0e2f62830ecc8",
+    "url": "/static/media/ShilajitSlide1.6a970a5a.png"
+  },
+  {
+    "revision": "d3722a303e6cf455e426db74478ac684",
+    "url": "/static/media/ShilajitSlide2.d3722a30.png"
+  },
+  {
+    "revision": "5f174263ee62d1d4481df4153fded979",
+    "url": "/static/media/ShilajitSlide3.5f174263.png"
+  },
+  {
+    "revision": "88b24fd579f8e6375f7c4c8e93e76259",
+    "url": "/static/media/ShilajitSlide4.88b24fd5.png"
+  },
+  {
+    "revision": "8c44447760eaa30e8dbcb6d3ba5aa1e9",
+    "url": "/static/media/ShilajitSlide5.8c444477.png"
+  },
+  {
+    "revision": "79e357365ff241da5a48fa8a2b788b6f",
+    "url": "/static/media/ShilajitSlide6.79e35736.png"
+  },
+  {
+    "revision": "2962db3641c276db6e5889686eb464a3",
+    "url": "/static/media/SpirulinaSlide1.2962db36.png"
+  },
+  {
+    "revision": "f8efe891ad2e24a4466f047bffadd900",
+    "url": "/static/media/SpirulinaSlide2.f8efe891.png"
+  },
+  {
+    "revision": "580b73435312f4abbadc8d55037f55b9",
+    "url": "/static/media/SpirulinaSlide3.580b7343.png"
+  },
+  {
+    "revision": "8bf29342bb5e796c8fb7adffbe87ebf7",
+    "url": "/static/media/SpirulinaSlide4.8bf29342.png"
+  },
+  {
+    "revision": "87395cea771ccfaca28d5333eb190c62",
+    "url": "/static/media/SpirulinaSlide5.87395cea.png"
+  },
+  {
+    "revision": "7e22a18bc3b4f89347fa106e76824911",
+    "url": "/static/media/SpirulinaSlide6.7e22a18b.png"
+  },
+  {
+    "revision": "7f9007aabfbdfd1ff62c5a4d3b3b72bc",
+    "url": "/static/media/TriphalaSlide1.7f9007aa.png"
+  },
+  {
+    "revision": "8b86dce833c412b9f9849725af378d2b",
+    "url": "/static/media/TriphalaSlide2.8b86dce8.png"
+  },
+  {
+    "revision": "13c688eebd0195c089bea4b06aa7577c",
+    "url": "/static/media/TriphalaSlide3.13c688ee.png"
+  },
+  {
+    "revision": "8058d59c92f765040e4ef2d2ffc0dd31",
+    "url": "/static/media/TriphalaSlide4.8058d59c.png"
+  },
+  {
+    "revision": "42a59eab0269cfb2f8100c371e8e96e7",
+    "url": "/static/media/TriphalaSlide5.42a59eab.png"
+  },
+  {
+    "revision": "b670a7c3d22557b6b8f2eb8735b548a3",
+    "url": "/static/media/TriphalaSlide6.b670a7c3.png"
+  },
+  {
+    "revision": "bb54a6f6079baca49cb3d107159b75d2",
+    "url": "/static/media/Untitled presentation.bb54a6f6.png"
+  },
+  {
+    "revision": "8cf3139654d770f9437ba126d174efdd",
+    "url": "/static/media/Website logo.8cf31396.PNG"
+  },
+  {
+    "revision": "fbb2ac3b261c53a49fa0e547260ee18a",
+    "url": "/static/media/abby-boggier-boadZKqd1YM-unsplash.fbb2ac3b.jpg"
+  },
+  {
+    "revision": "1961aa78fc08b5c2d3ce161f35339031",
+    "url": "/static/media/aboutUsBanner.1961aa78.jpg"
+  },
+  {
+    "revision": "ec18f67e83291472bfe9e5a59238904b",
+    "url": "/static/media/annie-spratt-JLpDkdcuceI-unsplash (1).ec18f67e.jpg"
+  },
+  {
+    "revision": "5bef6361d4754ce2bf0a12a25da96788",
+    "url": "/static/media/ashwagandha.5bef6361.png"
+  },
+  {
+    "revision": "41da0ffdb3a0ff69ba956b74deb01d1d",
+    "url": "/static/media/ayush-certification-500x500.41da0ffd.jpg"
+  },
+  {
+    "revision": "68c4d93aee8fbef8fce3d8d5fe41251e",
+    "url": "/static/media/blog1_heading.68c4d93a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8db3ba5947d6ff82fc5e7b4ac9169ce8",
+    "url": "/static/media/footerLogo.8db3ba59.png"
+  },
+  {
+    "revision": "6f799fa729229c6a2351ab19896d2382",
+    "url": "/static/media/hairWellness.6f799fa7.png"
+  },
+  {
+    "revision": "db518eed7f75bc1461e1294edd0654e2",
+    "url": "/static/media/hairWellnessSlide1.db518eed.png"
+  },
+  {
+    "revision": "cfa7ea97d10ff3619c0cd315550a7ed9",
+    "url": "/static/media/hairWellnessSlide2.cfa7ea97.png"
+  },
+  {
+    "revision": "70f804b4e81333ed5ce85fb980aa0de2",
+    "url": "/static/media/hairWellnessSlide3.70f804b4.png"
+  },
+  {
+    "revision": "a12a60e5bc19752db6cc3b2b0b60d070",
+    "url": "/static/media/hairWellnessSlide4.a12a60e5.png"
+  },
+  {
+    "revision": "27bba48f488c985a4b7c0e8a63bc2c22",
+    "url": "/static/media/hairWellnessSlide5.27bba48f.png"
+  },
+  {
+    "revision": "6f799fa729229c6a2351ab19896d2382",
+    "url": "/static/media/hairWellnessSlide6.6f799fa7.png"
+  },
+  {
+    "revision": "805b171aeab8c331fdc915cdfeaabe5b",
+    "url": "/static/media/hiclipart.com.805b171a.png"
+  },
+  {
+    "revision": "2424cfb844408351eb3010af46f06bc5",
+    "url": "/static/media/homeBanner1.2424cfb8.jpg"
+  },
+  {
+    "revision": "d5df1a6c24a44ed34d46bd6117e37a96",
+    "url": "/static/media/homeBanner2.d5df1a6c.jpg"
+  },
+  {
+    "revision": "289c3655fec9b07f53d14ec555fcb812",
+    "url": "/static/media/homeBanner3.289c3655.jpg"
+  },
+  {
+    "revision": "1f66687772ef22a7301aee8272cb2a38",
+    "url": "/static/media/homeBanner4.1f666877.jpg"
+  },
+  {
+    "revision": "39d67ff61c2d85b342ec5fac9c8110d7",
+    "url": "/static/media/homeBanner5.39d67ff6.jpg"
+  },
+  {
+    "revision": "1c890f4866ca195fa1793a48dedca29e",
+    "url": "/static/media/homeBannerResp1.1c890f48.jpg"
+  },
+  {
+    "revision": "7729cc6a0b6cb17f5c083fb54c920c2f",
+    "url": "/static/media/homeBannerResp2.7729cc6a.jpg"
+  },
+  {
+    "revision": "10c8373a5393d11354444fac61cb4d45",
+    "url": "/static/media/homeBannerResp3.10c8373a.jpg"
+  },
+  {
+    "revision": "b4b167b85ba4c2d1368a9ba3f8b91a58",
+    "url": "/static/media/homeBannerResp4.b4b167b8.jpg"
+  },
+  {
+    "revision": "882ffcba7213828b9636224b59983282",
+    "url": "/static/media/homeBannerResp5.882ffcba.jpg"
+  },
+  {
+    "revision": "f91027bdf00895e44dedae1d60891419",
+    "url": "/static/media/immunityBooster.f91027bd.png"
+  },
+  {
+    "revision": "7e5db3aba0ab2577753348e678897090",
+    "url": "/static/media/immunityBoosterSlide5.7e5db3ab.png"
+  },
+  {
+    "revision": "f91027bdf00895e44dedae1d60891419",
+    "url": "/static/media/immunityBoosterSlide6.f91027bd.png"
+  },
+  {
+    "revision": "02556b96cacc2d4cad3177cd7d09718a",
+    "url": "/static/media/immunityboosterSlide1.02556b96.png"
+  },
+  {
+    "revision": "66efdfd0c86f0bb45c609dede7ed2e3f",
+    "url": "/static/media/immunityboosterSlide2.66efdfd0.png"
+  },
+  {
+    "revision": "05d9602822cc187738b6fb3c36c3b718",
+    "url": "/static/media/immunityboosterSlide3.05d96028.png"
+  },
+  {
+    "revision": "c3698526da9f73a71209cdaefa41dd91",
+    "url": "/static/media/immunityboosterSlide4.c3698526.png"
+  },
+  {
+    "revision": "2f747cb589d411008d7290c303e8eb5e",
+    "url": "/static/media/location.2f747cb5.png"
+  },
+  {
+    "revision": "f80cdc3d611e43b96b34e1a6f5558754",
+    "url": "/static/media/luca-volpe-S1g3C9xVgVs-unsplash.f80cdc3d.jpg"
+  },
+  {
+    "revision": "6af5e637a5cceb180eb7ed606062e43b",
+    "url": "/static/media/mindWellness.6af5e637.png"
+  },
+  {
+    "revision": "dad100829a5236f9d64caf98ef912848",
+    "url": "/static/media/mindWellnessSlide1.dad10082.png"
+  },
+  {
+    "revision": "b3e17833ce0623a24a31023f8979fd3d",
+    "url": "/static/media/mindWellnessSlide2.b3e17833.png"
+  },
+  {
+    "revision": "d6fdd94f7c2018762d8430d0c90acc43",
+    "url": "/static/media/mindWellnessSlide3.d6fdd94f.png"
+  },
+  {
+    "revision": "45f33c6872b9f86a0676f6437acb6a29",
+    "url": "/static/media/mindWellnessSlide4.45f33c68.png"
+  },
+  {
+    "revision": "9dde0c0868d722c151c9c5b2064ec772",
+    "url": "/static/media/mindWellnessSlide5.9dde0c08.png"
+  },
+  {
+    "revision": "6af5e637a5cceb180eb7ed606062e43b",
+    "url": "/static/media/mindWellnessSlide6.6af5e637.png"
+  },
+  {
+    "revision": "79e357365ff241da5a48fa8a2b788b6f",
+    "url": "/static/media/shilajit.79e35736.png"
+  },
+  {
+    "revision": "072eae5738aba618352f70aff4d98696",
+    "url": "/static/media/skinWellness.072eae57.png"
+  },
+  {
+    "revision": "6592038d0fc369adce3a8f70064ad1d8",
+    "url": "/static/media/skinWellnessSlide1.6592038d.png"
+  },
+  {
+    "revision": "16d292ed3809735d693e050a653c7d32",
+    "url": "/static/media/skinWellnessSlide2.16d292ed.png"
+  },
+  {
+    "revision": "b05c053fe92fd405dbcb66dd75ae2975",
+    "url": "/static/media/skinWellnessSlide3.b05c053f.png"
+  },
+  {
+    "revision": "1956f168a28cbe6f5846375ebf05c591",
+    "url": "/static/media/skinWellnessSlide4.1956f168.png"
+  },
+  {
+    "revision": "b6c4a0098883076abb7e15d28aa4dc2f",
+    "url": "/static/media/skinWellnessSlide5.b6c4a009.png"
+  },
+  {
+    "revision": "072eae5738aba618352f70aff4d98696",
+    "url": "/static/media/skinWellnessSlide6.072eae57.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7e22a18bc3b4f89347fa106e76824911",
+    "url": "/static/media/spirulina.7e22a18b.png"
+  },
+  {
+    "revision": "b670a7c3d22557b6b8f2eb8735b548a3",
+    "url": "/static/media/triphala.b670a7c3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deploy-react/precache-manifest.eab24750fe79fccec752dbb672182011.js"
+  "/precache-manifest.7b246c6d012ebea68ddec3d55f1f8a33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deploy-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
